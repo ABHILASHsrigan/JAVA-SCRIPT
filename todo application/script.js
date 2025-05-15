64f9e8e6b916c92d26926b8920f72106331bdb80
ABHILASH SRIGAN <sriganabhilash@gmail.com>
2025-05-15 08:37:22 +0530

    Update script.js

--- todo application/script.js
+++ todo application/script.js
@@ -9,29 +9,4 @@ function addTask() {
     const currentTime = new Date();
     const timeString = currentTime.toLocaleTimeString();
 
-    listItem.innerHTML = `
-      <div class="task-left">
-        <input type="radio" onclick="toggleComplete(this)">
-        <div>
-          <div class="task-text">${taskText}</div>
-          <div class="timestamp">Added at: ${timeString}</div>
-        </div>
-      </div>
-      <button onclick="removeTask(this)">❌</button>
-    `;
 
-    // Add new task at the top
-    taskList.insertBefore(listItem, taskList.firstChild);
-
-    document.getElementById('taskInput').value = '';
-  }
-
-  function removeTask(button) {
-    const taskItem = button.parentElement;
-    taskItem.remove();
-  }
-
-  function toggleComplete(radio) {
-    const taskText = radio.nextElementSibling.querySelector('.task-text');
-    taskText.classList.toggle('completed');
-  }
