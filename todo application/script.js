915865335273a3b3f1ec67e61db7d6dea8f2feab
ABHILASH SRIGAN <sriganabhilash@gmail.com>
2025-05-15 08:37:39 +0530

    Update script.js

--- todo application/script.js
+++ todo application/script.js
@@ -1,12 +1,1 @@
 
-function addTask() {
-    const taskText = document.getElementById('taskInput').value.trim();
-    if (taskText === '') return;
-
-    const taskList = document.getElementById('taskList');
-    const listItem = document.createElement('li');
-
-    const currentTime = new Date();
-    const timeString = currentTime.toLocaleTimeString();
-
-
